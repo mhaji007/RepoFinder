006d7e8644dfd065ad0776a331da1493c80ade4c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-23 19:10:54 -0400

    Embed text input and select components in search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import TextInput from './shared/text-input';
+import Select from './shared/select';
 
-const Search = () => {
+const Search = (props) => {
+    
+    const {language, searchText, onSearchTextChange, onLanguageChange} = props;
+    
+    // Options used in select
+    const language = []
+    
     return (
+        
         <div>
-            <TextInput/>
-            <Select/>
+            <TextInput
+            label = "Repo Search"
+            value={searchText}
+            onChange={(value) => onSearchTextChange(value)}/>
+
+            <Select
+            lable="language"
+            value={language}
+            options={languages}
+            onChange={(value) => onLanguageChange(value)}
+            />
+
         </div>
-    )
+
+    );
 }
 
 export default Search;
