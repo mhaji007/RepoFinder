51b1e60a9db92baeaebca692f4ab34273e8f8df0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-25 13:38:50 -0400

    Style navbar using Bulma

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import Link from 'next/link';
 
- const Navbar = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link href="/index" as="/index">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about" as="/about">
-                        <a>About</a>
-                    </Link>
-                </li>
-            </ul>
+const Navbar = () => {
+  return (
+    <nav
+      class="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <Link href="/">
+          <a class="navbar-item">Repo Finder 🕵</a>
+        </Link>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <Link href="/">
+            <a class="navbar-item">Home</a>
+          </Link>
+          <Link href="/about">
+            <a class="navbar-item">About</a>
+          </Link>
         </div>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,24 @@
 import Navbar from '../components/navbar';
+import '../scss/styles.scss';
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}) {
     return (
 
         <div>
+
+            <Head>
+                <title>Repo Finder</title>
+            </Head>
+
             <Navbar></Navbar>
+            
+            <div className="page">
 
             <Component {...pageProps}/>
+
+            </div>
+
 
         </div>
 
