7429f3ae19af98436c313fe7cf689723ba3ff57c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-25 16:36:14 -0400

    Align image with input and select fields

--- src/components/search.js
+++ src/components/search.js
@@ -2,34 +2,30 @@ import React from 'react';
 import TextInput from './shared/text-input';
 import Select from './shared/select';
 import LANGUAGES from '../constants/languages.constant';
+import styles from './search.module.scss';
 
 const Search = (props) => {
-    
-    const {language, searchText, onSearchTextChange, onLanguageChange} = props;
-    
-    // Options used in select
-    const languages = [{ value: '', label: 'All' }, ...LANGUAGES]
-    
-    return (
-        
-        <div>
-            <TextInput
-            label = "Repo Search"
-            value={searchText}
-            onChange={(value) => onSearchTextChange(value)}/>
+  const { language, searchText, onSearchTextChange, onLanguageChange } = props;
 
-            <Select
-            lable="language"
-            value={language}
-            options={languages}
-            onChange={(value) => onLanguageChange(value)}
-            />
+  const languages = [{ value: '', label: 'All' }, ...LANGUAGES];
 
-        </div>
-
-    );
-}
+  return (
+    <div className={styles.search}>
+      <TextInput
+        className={styles.searchInput}
+        label="Repo Search"
+        value={searchText}
+        onChange={(value) => onSearchTextChange(value)}
+      />
+      <Select
+        className={styles.languageSelect}
+        label="Language"
+        value={language}
+        options={languages}
+        onChange={(value) => onLanguageChange(value)}
+      />
+    </div>
+  );
+};
 
 export default Search;
-
-
