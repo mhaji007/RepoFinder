3641857877d0aca4f0cd61c4e50f5cdc0008d7af
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-25 19:20:44 -0400

    Style RepoListItem component

--- src/components/repo.list.item.js
+++ src/components/repo.list.item.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './repo-list-item.module.scss';
 
 const RepoListItem = ({repo}) => {
+
+    const cutDescription = (description) => {
+
+        if(description.length > 100) {
+            return description.slice(0, 100) + '...';
+        }
+
+        return description
+    }
+
     return (
-        <div>
-            <div>
+        <div className={`${styles.repoListItem} box`}>
+            <div className={styles.repoName}>
                 <Link href="/repo/[id]" as={`/repo/${repo.id}`}>
-                    <a>{repo.name}</a>
+                    <a >{repo.name}</a>
                 </Link>
-                <p>{repo.description}</p>
+            </div>
+                <p className={styles.className}>{cutDescription(repo.description)}</p>
+        
+            <div className={styles.footer}>
+                <div className={styles.counters}>
+                    <div className={styles.counter}>
+                    
+                    <span>{repo.stargazers_count}</span>
+
+                    </div>
+                    <div className={styles.counter}>
+                    
+                    <span>{repo.forks_count}</span>
+
+                    </div>
+                </div>
+                <div className={styles.language}>
+                    <span>{repo.language}</span>
+                </div>
             </div>
         </div>
     );
